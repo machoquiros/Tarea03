e67f015186e4b039d584e6bc45469335977b0a95
Michael <Michael@Minerva>
2022-03-03 18:31:34 -0600

    Tarea 03 primer commit

---
+++ src/main/java/com/tarea03/Tarea03Application.java
@@ -0,0 +1,13 @@
+package com.tarea03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea03Application.class, args);
+	}
+
+}
